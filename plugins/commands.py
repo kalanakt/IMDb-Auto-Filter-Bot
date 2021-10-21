80ae2aefbdf273e709af32a56188d7d8165f88fd
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-10-22 04:49:55 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -101,7 +101,6 @@ async def start(bot, cmd):
             reply_markup=reply_markup,
             parse_mode="html",
             reply_to_message_id=update.message_id
-        )
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
